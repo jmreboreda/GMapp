cf4ff04a5f5b70a02379850c130e312a756ccb4d
jmreboreda <jmreboreda@mundo-r.com>
2016-01-30 19:42:24 +0100

    Foreach and Map

---
+++ src/main/java/com/gmapp/utilities/Borrame.java
@@ -0,0 +1,14 @@
+/*
+ * RBJM
+ * Aplicación desarrollada por José M. Reboreda Barcia
+ * para uso propio en Gestoría MOLDES.
+ */
+package com.gmapp.utilities;
+
+/**
+ *
+ * @author jmrb
+ */
+public class Borrame {
+    
+}
